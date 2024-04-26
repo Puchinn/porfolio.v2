a29a2da11e471001dc9416abfb05589215f48989
Esteban Sayago <sayagoo823@gmail.com>
2024-04-26 19:35:15 -0300

    fix: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 import robotsTxt from "astro-robots-txt";
 
@@ -15,4 +16,5 @@ export default defineConfig({
     },
   },
   output: "server",
+  adapter: vercel(),
 });
